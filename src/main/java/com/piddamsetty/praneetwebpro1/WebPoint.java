8eb3817ed531c95ef43e51c799e6b39b8e4050b4
praneet <vasu6657@gmail.com>
2018-08-04 14:35:35 -0700

    Converted to Pi Project

--- src/main/java/com/piddamsetty/praneetwebpro1/WebPoint.java
+++ src/main/java/com/piddamsetty/praneetwebpro1/WebPoint.java
@@ -1,7 +1,5 @@
 package com.piddamsetty.praneetwebpro1;
 
-import com.piddamsetty.praneetwebpro1.services.FileServices;
-import com.piddamsetty.praneetwebpro1.services.PiServices;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,59 +12,11 @@ import java.util.Random;
 @RestController
 public class WebPoint {
 
-    FileServices fileServices = new FileServices();
-    PiServices piServices = new PiServices();
-    String template = fileServices.resourceFileRead("template");
-
-    public String applyTemplate(String content) {
-        String t1 = template.replace("##leftLinks##","<li><a href=\"#\">/</a></li>");
-        return t1.replace("##bodycontent##",content);
-    }
-
     @RequestMapping("/")
     public String root() {
         System.out.println("New request for home");
-        String content = fileServices.readFromInputStream("index.html");
-        return applyTemplate(content);
+        String content = "index.html";
+        return content;
     }
-
-    @RequestMapping("/blog/{page}")
-    public String getPage(@PathVariable String page) {
-        System.out.println("New request to get blog for page: "+page);
-        return fileServices.resourceFileRead(page);
-    }
-
-    @RequestMapping("/friends")
-    public String getListOfMyFriends() {
-        return "A,B,C,D";
-    }
-
-    @RequestMapping("/random")
-    public int findRandomInteger() {
-        Random number = new Random();
-        return number.nextInt();
-    }
-
-    @RequestMapping("/calculate/{operation}/{i}/{j}")
-    public Integer calculate(@PathVariable Integer i, @PathVariable Integer j, @PathVariable String operation) {
-        if(operation.toLowerCase().equals("multiply")) {
-            return i*j;
-        }
-        if(operation.toLowerCase().equals("add")) {
-            return i+j;
-        }
-        if(operation.toLowerCase().equals("divide")) {
-            return i/j;
-        }
-        if(operation.toLowerCase().equals("substract")) {
-            return i-j;
-        }
-        return null;
-    }
-
-//    @RequestMapping("/pi/on/{i}")
-//    public void pinon(@PathVariable Integer i) {
-//        piServices.turnOn(i);
-//    }
 
 }
